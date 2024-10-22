34de1f4ba7cc2589ea88149613ea7c9a8f55103c
Md. Musfiqur Rahman <projion@gmail.com>
2024-10-22 18:52:38 +0600

    api get consumed and show on table without service

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+  //roleList: any[] = [];
+  roleList: IRole[] = [];
+
+  //constructor(private http: HttpClient) {   // old way to DI HttpClient
+  //}
+  http = inject(HttpClient);  // new way to DI HttpClient
+
+  ngOnInit(): void {
+    //alert('Method not implemented. from ngOnInit');
+    this.getAllRoles();
+  }
+
+  getAllRoles() {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res: any) => {
+      this.roleList = res.data;
+    })
+  }
 
 }
 
