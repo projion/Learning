e3df228af97380d0e1586c567b8f170cfeefcddd
Md. Musfiqur Rahman <projion@gmail.com>
2024-10-23 17:16:23 +0600

    if, else, loader, and route added prev commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MasterComponent } from './components/master/master.component';
 import { BasicPracticesComponent } from './components/basic-practices/basic-practices.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MasterComponent, BasicPracticesComponent],
+  imports: [MasterComponent, BasicPracticesComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -13,13 +13,16 @@ import { MasterService } from '../../services/master.service';
 export class DesignationComponent implements OnInit {
 
   designationList: IDesignation[] = [];
+  isLoader: boolean = true;
   masterService = inject(MasterService);
   ngOnInit(): void {
     //alert('Method not implemented.DesignationComponent');
     this.masterService.getDesignations().subscribe((result: APIResponseModel) => {
       this.designationList = result.data;
+      this.isLoader = false;
     }, error => {
       alert("API error / Network Down")
+      this.isLoader = false;
     }
     )
   }
